0216ca6e545de2bf0fffcdb96a4953b03d6d62d6
Hwrng2 <hwrng2@Hwrng2ui-MacBookPro.local>
2020-07-17 22:36:30 +0900

    etc: initial setting
    - 사용 할 패키지 설치 및 사용하지 않는 패키지 삭제
    (react, styled-components 관련 패키지 설치 및 testing 관련 패키지 삭제)
    - 프로젝트 디렉토리 구조 생성
    - GlobalStyle 정의

---
+++ src/components/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// routing code 추가
+const App = () => {
+    return <div>App</div>
+}
+
+export default App
---
+++ src/components/home/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import * as S from './HomeStyle'
+
+const Home = () => {
+    return <S.Wrapper>Home</S.Wrapper>
+}
+
+export default Home
